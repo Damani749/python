8ddaff95561c40cd2e1e4b9318898e6f2d793b6d
Damani749 <damani.ramya309@gmail.com>
2022-01-20 10:36:32 +0530

    jenkins.test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def add (x,y):
+	z = x + y
+	print ("The addition is: ",z)
+	return z
+	
+
+if __name__ =="__main__":
+	print ("Enter any two integers")
+	a = int (input())
+	b = int (input())
+	add (a,b)
+
